d343fa652c8d953bd3c93fb368bdd310871e6e91
Saransh Sharma <saransh@theupscale.in>
2019-06-01 23:49:51 +0530

    New code

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,7 +16,7 @@ docker-compose -f docker-compose.yml -f docker-compose.setup.yml up setup_elasti
 # setup kibana and logstash (and system passwords)
 docker-compose -f docker-compose.yml -f docker-compose.setup.yml up setup_kibana setup_logstash
 # setup beats and apm server
-docker-compose -f docker-compose.yml -f docker-compose.setup.yml up setup_auditbeat setup_filebeat setup_heartbeat setup_metricbeat setup_packetbeat setup_apm_server
+docker-compose -f docker-compose.yml -f docker-compose.setup.yml up setup_filebeat setup_mongo setup_redis
 
 printf "Setup completed successfully. To start the stack please run:\n\t docker-compose up -d\n"
 printf "\nIf you wish to remove the setup containers please run:\n\tdocker-compose -f docker-compose.yml -f docker-compose.setup.yml down --remove-orphans\n"
